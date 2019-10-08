032fa673b372bb0b53a13bafd37fd139c1bde478
Pratheesh <pratheeshrussell@gmail.com>
2019-10-08 13:51:46 +0530

    Add project files.

---
+++ Creo_get_table/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(80, 21)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 92)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(466, 458)
+        Me.TabControl1.TabIndex = 1
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.ListView1)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.ComboBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(458, 429)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Tables"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(458, 429)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "About"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Drawing Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Model Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Tables"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(266, 21)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Rows:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(266, 55)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Columns:"
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Row"
+        Me.ColumnHeader1.Width = 100
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Column"
+        Me.ColumnHeader2.Width = 100
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Text"
+        Me.ColumnHeader3.Width = 200
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(15, 95)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(422, 309)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(26, 34)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(389, 124)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Program to Extract Table data from a Creo Drawing" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "By Pratheesh Russell.S"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(486, 580)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tables Data Extraction"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Creo_get_table/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports pfcls
+Imports System.IO
+Public Class Form1
+    'https://community.ptc.com/t5/Data-Exchange/Creo-VBA-API-for-drawing-table-update/td-p/397136
+    'read last comment
+    'https://community.ptc.com/t5/Detailing-MBD-MBE/Extracting-Dimension-from-Creo-model/td-p/217712
+    Dim Casync As New pfcls.CCpfcAsyncConnection
+    Dim asyncConnection As IpfcAsyncConnection
+    Dim cAC As New CCpfcAsyncConnection
+    Dim session As IpfcBaseSession
+    Dim model1 As IpfcModel2D
+    Dim name1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        On Error GoTo a
+        ConnectToCreo()
+        Label1.Text = "Drawing Name: " & name1
+        Dim mainmodel As IpfcModel
+        For Each modelname As IpfcModel In model1.ListModels()
+            mainmodel = modelname
+            Label2.Text = "Model Name: " & modelname.FileName
+        Next
+        GetTablesList()
+
+        ' GoTo a
+
+        Dim Drawing As IpfcDrawing = CType(model1, IpfcDrawing)
+
+        Dim TotalSheets As Integer
+
+        TotalSheets = Drawing.NumberOfSheets
+
+        Dim modowner As IpfcModelItemOwner = CType(model1, IpfcModelItemOwner)
+        Dim symbol2 As IpfcDetailItems = modowner.ListDetailItems(EpfcDetailType.EpfcDETAIL_NOTE, 2)
+        MsgBox(symbol2.Item(0).Count)
+        Dim p12 = ""
+        For Each notetext As IpfcDetailNoteItem In symbol2
+            '  MsgBox("here")
+            '  Dim mynote As IpfcNote = CType(notetext, IpfcNote)
+            'MsgBox(mynote.GetText(False))
+        Next
+
+        ' For i = 1 To TotalSheets ' Note here i is the required sheet number
+
+        Dim owner As IpfcDetailItemOwner = CType(model1, IpfcDetailItemOwner)
+
+
+        Dim symbol As IpfcDetailItems = owner.ListDetailItems(EpfcDetailType.EpfcDETAIL_NOTE, 2)
+
+
+        DisconnectCreo()
+
+        'MessageBox.Show(symbol.GetInstructions.FullPath)
+
+        ' Next
+
+
+
+        GoTo a
+
+a:
+        DisconnectCreo()
+
+
+    End Sub
+    Private Sub GetTablesList()
+        Dim tables As IpfcTables
+        tables = model1.ListTables()
+        Dim p = 0
+        For Each singletable As IpfcTable In tables
+            ComboBox1.Items.Add(p)
+        Next
+    End Sub
+    Private Sub GetTablesVal()
+
+        Dim tables As IpfcTables
+        tables = model1.ListTables()
+        Dim noRow
+        Dim noCol
+        Dim tableno = CInt(ComboBox1.Text)
+        Dim singletable As IpfcTable = tables(tableno)
+
+        noRow = singletable.GetRowCount
+        noCol = singletable.GetColumnCount
+        Label4.Text = "Rows: " & noRow
+        Label5.Text = "Columns: " & noCol
+
+        Dim tableCellCreate As New pfcls.CCpfcTableCell
+        Dim tableCell As pfcls.IpfcTableCell
+        ListView1.Items.Clear()
+        For i = 1 To noRow
+            For j = 1 To noCol
+                Dim cellgettext
+                Dim celltext = ""
+                tableCell = tableCellCreate.Create(i, j)
+                Try
+                    cellgettext = singletable.GetText(tableCell, 0)
+                    celltext = cellgettext.Item(0).ToString
+                Catch ex As Exception
+                    ' MsgBox(ex.Message)
+                End Try
+
+                Dim lvw As ListView = ListView1
+                Dim newItem As ListViewItem
+                newItem = lvw.Items.Add(i)
+                newItem.SubItems.Add(j)
+                newItem.SubItems.Add(celltext)
+
+
+            Next
+        Next
+        Exit Sub
+
+    End Sub
+    Private Sub ConnectToCreo()
+        asyncConnection = cAC.Connect("", "", DBNull.Value, DBNull.Value)
+        session = asyncConnection.Session
+        name1 = session.CurrentModel.FileName
+
+        model1 = session.GetModel(name1, EpfcModelType.EpfcMDL_DRAWING)
+    End Sub
+    Private Sub DisconnectCreo()
+        asyncConnection.Disconnect(1)
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ConnectToCreo()
+        GetTablesVal()
+        DisconnectCreo()
+    End Sub
+End Class
